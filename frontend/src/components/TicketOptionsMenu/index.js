8203d78558a0aaf6444c9820f25dbc51230bd69b
Natã <natasjesus1@gmail.com>
2024-07-15 14:09:05 -0300

    name

--- frontend/src/components/TicketOptionsMenu/index.js
+++ frontend/src/components/TicketOptionsMenu/index.js
@@ -14,114 +14,104 @@ import { AuthContext } from "../../context/Auth/AuthContext";
 import ScheduleModal from "../ScheduleModal";
 
 const TicketOptionsMenu = ({ ticket, menuOpen, handleClose, anchorEl }) => {
-	const [confirmationOpen, setConfirmationOpen] = useState(false);
-	const [transferTicketModalOpen, setTransferTicketModalOpen] = useState(false);
-	const isMounted = useRef(true);
-	const { user } = useContext(AuthContext);
+    const [confirmationOpen, setConfirmationOpen] = useState(false);
+    const [transferTicketModalOpen, setTransferTicketModalOpen] = useState(false);
+    const isMounted = useRef(true);
+    const { user } = useContext(AuthContext);
 
-	const [scheduleModalOpen, setScheduleModalOpen] = useState(false);
-	const [contactId, setContactId] = useState(null);
+    const [scheduleModalOpen, setScheduleModalOpen] = useState(false);
+    const [contactId, setContactId] = useState(null);
 
-	useEffect(() => {
-		return () => {
-			isMounted.current = false;
-		};
-	}, []);
+    useEffect(() => {
+        return () => {
+            isMounted.current = false;
+        };
+    }, []);
 
-	const handleDeleteTicket = async () => {
-		try {
-			await api.delete(`/tickets/${ticket.id}`);
-		} catch (err) {
-			toastError(err);
-		}
-	};
+    const handleDeleteTicket = async () => {
+        try {
+            await api.delete(`/tickets/${ticket.id}`);
+        } catch (err) {
+            toastError(err);
+        }
+    };
 
-	const handleOpenConfirmationModal = e => {
-		setConfirmationOpen(true);
-		handleClose();
-	};
+    const handleOpenConfirmationModal = (e) => {
+        setConfirmationOpen(true);
+        handleClose();
+    };
 
-	const handleOpenTransferModal = e => {
-		setTransferTicketModalOpen(true);
-		handleClose();
-	};
+    const handleOpenTransferModal = (e) => {
+        setTransferTicketModalOpen(true);
+        handleClose();
+    };
 
-	const handleCloseTransferTicketModal = () => {
-		if (isMounted.current) {
-			setTransferTicketModalOpen(false);
-		}
-	};
+    const handleCloseTransferTicketModal = () => {
+        if (isMounted.current) {
+            setTransferTicketModalOpen(false);
+        }
+    };
 
-	const handleOpenScheduleModal = () => {
-		handleClose();
-		setContactId(ticket.contact.id);
-		setScheduleModalOpen(true);
-	}
+    const handleOpenScheduleModal = () => {
+        handleClose();
+        setContactId(ticket.contact.id);
+        setScheduleModalOpen(true);
+    };
 
-	const handleCloseScheduleModal = () => {
-		setScheduleModalOpen(false);
-		setContactId(null);
-	}
+    const handleCloseScheduleModal = () => {
+        setScheduleModalOpen(false);
+        setContactId(null);
+    };
 
-	return (
-		<>
-			<Menu
-				id="menu-appbar"
-				anchorEl={anchorEl}
-				getContentAnchorEl={null}
-				anchorOrigin={{
-					vertical: "bottom",
-					horizontal: "right",
-				}}
-				keepMounted
-				transformOrigin={{
-					vertical: "top",
-					horizontal: "right",
-				}}
-				open={menuOpen}
-				onClose={handleClose}
-			>
-				<MenuItem onClick={handleOpenScheduleModal}>
-					{i18n.t("ticketOptionsMenu.schedule")}
-				</MenuItem>
-				<MenuItem onClick={handleOpenTransferModal}>
-					{i18n.t("ticketOptionsMenu.transfer")}
-				</MenuItem>
-				<Can
-					role={user.profile}
-					perform="ticket-options:deleteTicket"
-					yes={() => (
-						<MenuItem onClick={handleOpenConfirmationModal}>
-							{i18n.t("ticketOptionsMenu.delete")}
-						</MenuItem>
-					)}
-				/>
-			</Menu>
-			<ConfirmationModal
-				title={`${i18n.t("ticketOptionsMenu.confirmationModal.title")}${
-					ticket.id
-				} ${i18n.t("ticketOptionsMenu.confirmationModal.titleFrom")} ${
-					ticket.contact.name
-				}?`}
-				open={confirmationOpen}
-				onClose={setConfirmationOpen}
-				onConfirm={handleDeleteTicket}
-			>
-				{i18n.t("ticketOptionsMenu.confirmationModal.message")}
-			</ConfirmationModal>
-			<TransferTicketModalCustom
-				modalOpen={transferTicketModalOpen}
-				onClose={handleCloseTransferTicketModal}
-				ticketid={ticket.id}
-			/>
-			<ScheduleModal
-				open={scheduleModalOpen}
-				onClose={handleCloseScheduleModal}
-				aria-labelledby="form-dialog-title"
-				contactId={contactId}
-			/>
-		</>
-	);
+    return (
+        <>
+            <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                getContentAnchorEl={null}
+                anchorOrigin={{
+                    vertical: "bottom",
+                    horizontal: "right",
+                }}
+                keepMounted
+                transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                }}
+                open={menuOpen}
+                onClose={handleClose}
+            >
+                <MenuItem onClick={handleOpenScheduleModal}>{i18n.t("ticketOptionsMenu.schedule")}</MenuItem>
+                <MenuItem onClick={handleOpenTransferModal}>{i18n.t("ticketOptionsMenu.transfer")}</MenuItem>
+                <Can
+                    role={user.profile}
+                    perform="ticket-options:deleteTicket"
+                    yes={() => (
+                        <MenuItem onClick={handleOpenConfirmationModal}>{i18n.t("ticketOptionsMenu.delete")}</MenuItem>
+                    )}
+                />
+            </Menu>
+            <ConfirmationModal
+                title={`${i18n.t("ticketOptionsMenu.confirmationModal.title")} ${ticket.contact.name} #${ticket.id}?`}
+                open={confirmationOpen}
+                onClose={setConfirmationOpen}
+                onConfirm={handleDeleteTicket}
+            >
+                {i18n.t("ticketOptionsMenu.confirmationModal.message")}
+            </ConfirmationModal>
+            <TransferTicketModalCustom
+                modalOpen={transferTicketModalOpen}
+                onClose={handleCloseTransferTicketModal}
+                ticketid={ticket.id}
+            />
+            <ScheduleModal
+                open={scheduleModalOpen}
+                onClose={handleCloseScheduleModal}
+                aria-labelledby="form-dialog-title"
+                contactId={contactId}
+            />
+        </>
+    );
 };
 
 export default TicketOptionsMenu;
