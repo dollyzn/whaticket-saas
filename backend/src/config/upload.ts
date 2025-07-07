cf6c2173728f6573ee288d8dcf5bfb38660ffd3c
Natã <natasjesus1@gmail.com>
2025-07-07 00:34:00 -0300

    fix: corrigir formatação do caminho e nome do arquivo no armazenamento de mídia

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import multer from "multer";
 import fs from "fs";
+import { randomBytes } from "crypto";
 
 const publicFolder = path.resolve(__dirname, "..", "..", "public");
 
@@ -8,31 +9,34 @@ export default {
   directory: publicFolder,
   storage: multer.diskStorage({
     destination: async function (req, file, cb) {
-
-      const { typeArch, fileId } = req.body;      
+      const { typeArch, fileId } = req.body;
 
       let folder;
 
       if (typeArch && typeArch !== "announcements") {
-        folder =  path.resolve(publicFolder , typeArch, fileId ? fileId : "") 
+        folder = path.resolve(publicFolder, typeArch, fileId ? fileId : "");
       } else if (typeArch && typeArch === "announcements") {
-        folder =  path.resolve(publicFolder , typeArch) 
-      }
-      else
-      {
-        folder =  path.resolve(publicFolder) 
+        folder = path.resolve(publicFolder, typeArch);
+      } else {
+        folder = path.resolve(publicFolder);
       }
 
       if (!fs.existsSync(folder)) {
-        fs.mkdirSync(folder,  { recursive: true })
-        fs.chmodSync(folder, 0o777)
+        fs.mkdirSync(folder, { recursive: true });
+        fs.chmodSync(folder, 0o777);
       }
       return cb(null, folder);
     },
-    filename(req, file, cb) {
-      const { typeArch } = req.body;
+    filename(_, file, cb) {
+      const ext = path.extname(file.originalname);
+      const baseName = path
+        .basename(file.originalname, ext)
+        .replace("/", "-")
+        .replace(/ /g, "_");
 
-      const fileName = typeArch && typeArch !== "announcements" ? file.originalname.replace('/','-').replace(/ /g, "_") : new Date().getTime() + '_' + file.originalname.replace('/','-').replace(/ /g, "_");
+      const uniqueSuffix = `${Date.now()}-${randomBytes(4).toString("hex")}`;
+      const fileName = `${baseName}-${uniqueSuffix}${ext}`;
+
       return cb(null, fileName);
     }
   })
