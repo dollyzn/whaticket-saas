a6c240b350fddcc4695133244a2a708ab992bb20
Natã <natasjesus1@gmail.com>
2025-10-06 09:52:13 -0300

    fix: adicionar verificação para ignorar JIDs de newsletters na inicialização do WASocket

--- backend/src/libs/wbot.ts
+++ backend/src/libs/wbot.ts
@@ -5,7 +5,8 @@ import makeWASocket, {
   DisconnectReason,
   fetchLatestBaileysVersion,
   makeCacheableSignalKeyStore,
-  isJidBroadcast
+  isJidBroadcast,
+  isJidNewsletter
 } from "baileys";
 
 import Whatsapp from "../models/Whatsapp";
@@ -98,7 +99,7 @@ export const initWASocket = async (whatsapp: Whatsapp): Promise<Session> => {
           },
           version: [2, 3000, 1023223821],
           msgRetryCounterCache,
-          shouldIgnoreJid: jid => isJidBroadcast(jid)
+          shouldIgnoreJid: jid => isJidBroadcast(jid) || isJidNewsletter(jid)
         });
 
         wsocket.ev.on(
